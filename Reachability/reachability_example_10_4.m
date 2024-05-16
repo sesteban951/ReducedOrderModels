be2b867211ab602a3289dbd4be22a4a93103d5d8
sesteban951 <sesteban@caltech.edu>
2024-05-16 11:55:51 -0700

    fixed example 4 and 9 and finished almost all Ch10 examples

--- Reachability/reachability_example_10_4.m
+++ Reachability/reachability_example_10_4.m
@@ -26,11 +26,9 @@ h_x = [10; 10; 10; 10];
 H_u = [1; 
       -1];
 h_u = [5; 5];
-U = [-5; 5];
+U = [5; -5];
 
 % ---------------------- Forwards Reachability -----------------------------
-% I COULDN'T GET THE RESULTS IN THE BOOK.
-
 figure(); grid on; hold on;
 
 % plot the orignal intial condition set
@@ -40,28 +38,97 @@ p_X0 = polyshape(X0);
 plot(p_X0);
 
 % plot the one step reachable set
-AX = X0 * A'
+AX = X0 * A';
+[AX_row, ~] = size(AX);
 BU = U * B';
-AX_sum_BU = [AX + ones(size(AX,1),1) * BU(1,:);
-             AX + ones(size(AX,1),1) * BU(2,:)];
+[BU_row, ~] = size(BU);
+
+AX_sum_BU = [];
+for i = 1:AX_row
+    for j = 1:BU_row
+        AX_sum_BU = [AX_sum_BU; AX(i,:) + BU(j,:)]
+    end
+end
+
+AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
+
+[C, d] = vert2con(AX_sum_BU)
+[row, ~] = size(C);
+C = [C; H_x];
+d = [d; h_x];
+AX_sum_BU = con2vert(C, d);
 AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
 p_AX_sum_BU = polyshape(AX_sum_BU);
 plot(p_AX_sum_BU);
 
-% do another one
-AX = AX_sum_BU * A';
+% plot another one!
+AX =  AX_sum_BU * A';
+[AX_row, ~] = size(AX);
 BU = U * B';
-AX_sum_BU = [AX + ones(size(AX,1),1) * BU(1,:);
-             AX + ones(size(AX,1),1) * BU(2,:)];     
+[BU_row, ~] = size(BU);
+
+AX_sum_BU = [];
+for i = 1:AX_row
+    for j = 1:BU_row
+        AX_sum_BU = [AX_sum_BU; AX(i,:) + BU(j,:)]
+    end
+end
+
+AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
+[C, d] = vert2con(AX_sum_BU)
+[row, ~] = size(C);
+C = [C; H_x];
+d = [d; h_x];
+AX_sum_BU = con2vert(C, d);
 AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
 p_AX_sum_BU = polyshape(AX_sum_BU);
 plot(p_AX_sum_BU);
 
-% do another one
-AX = AX_sum_BU * A';
+% plot another one!
+AX =  AX_sum_BU * A';
+[AX_row, ~] = size(AX);
 BU = U * B';
-AX_sum_BU = [AX + ones(size(AX,1),1) * BU(1,:);
-             AX + ones(size(AX,1),1) * BU(2,:)];     
+[BU_row, ~] = size(BU);
+
+AX_sum_BU = [];
+for i = 1:AX_row
+    for j = 1:BU_row
+        AX_sum_BU = [AX_sum_BU; AX(i,:) + BU(j,:)]
+    end
+end
+
+AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
+[C, d] = vert2con(AX_sum_BU)
+[row, ~] = size(C);
+C = [C; H_x];
+d = [d; h_x];
+AX_sum_BU = con2vert(C, d);
 AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
 p_AX_sum_BU = polyshape(AX_sum_BU);
 plot(p_AX_sum_BU);
+
+% plot another one!
+AX =  AX_sum_BU * A';
+[AX_row, ~] = size(AX);
+BU = U * B';
+[BU_row, ~] = size(BU);
+
+AX_sum_BU = [];
+for i = 1:AX_row
+    for j = 1:BU_row
+        AX_sum_BU = [AX_sum_BU; AX(i,:) + BU(j,:)]
+    end
+end
+
+AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
+[C, d] = vert2con(AX_sum_BU)
+[row, ~] = size(C);
+C = [C; H_x];
+d = [d; h_x];
+AX_sum_BU = con2vert(C, d);
+AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
+p_AX_sum_BU = polyshape(AX_sum_BU);
+plot(p_AX_sum_BU);
+
+% --------------------------------------------------------------------
+legend(["$\mathcal X_0$", "$\mathcal R_1 (\mathcal X_0)$", "$\mathcal R_2 (\mathcal X_0)$", "$\mathcal R_3 (\mathcal X_0)$", "$\mathcal R_4 (\mathcal X_0)$"], 'interpreter', 'latex');
--- Reachability/reachability_example_10_9.m
+++ Reachability/reachability_example_10_9.m
@@ -11,7 +11,7 @@ B = [1; 0];
 % input constraint set, Ucal
 H_u = [1; -1];
 h_u = 5.0 * [1; 1];
-U = [-5; 5];
+U = [5; -5];
 
 % state constraint set, Xcal
 H_x = [1, 0;
@@ -21,22 +21,26 @@ H_x = [1, 0;
 h_x = 10 * [1; 1; 1; 1];
 
 % distrubance set, Wcal
-H_w = [1, 0'
+H_w = [1, 0;
        0, 1;
       -1, 0;
        0, -1];
 h_w = 1.0 * [1; 1; 1; 1];
 
-% -------------------------- Forward Reachability ---------------------------
+% -------------------------- Backwards Reachability ---------------------------
 figure(); hold on; grid on; axis equal;
+
+% plot X
 X = con2vert(H_x, h_x);
 X = X(convhull(X),:);
 p_X = polyshape(X);
 plot(p_X);
 
 % Compute one step Pre(X, U, W), Eq. (10.49)
-H = [H_x * A, H_x * B;
-     zeros(size(H_u, 1), size(H_x, 2)), H_u];
+HA = H_x * A;
+HB = H_x * B;
+H = [HA,                               HB;
+     zeros(size(H_u, 1), size(HA, 2)), H_u];
 
 % solve the linear program (Eq. 10.50)
 % h_i_tilde = min (h_i - H_i * w) subject to  H_w * w <= h_w
@@ -69,31 +73,44 @@ V_x = V_x(convhull(V_x),:);
 p_V_x = polyshape(V_x);
 plot(p_V_x);
 
-legend(["$\mathcal{X}$", "$Suc(\mathcal{X}, \mathcal{U}, \mathcal{W})$", "$Suc(\mathcal{X}, \mathcal{U}, \mathcal{W}) \cap \mathcal{X}$"], 'interpreter', 'latex');
 
+% -------------------------- Forward Reachability ---------------------------
+% figure(); hold on; grid on; axis equal;
 
 % compute  Suc(X, U, W)
 AX = X * A';
 AX = unique(AX, 'rows');
-W = con2vert(H_w, h_w);
+[AX_row, AX_col] = size(AX);
 
-AX_sum_W = [];
-for i = 1:size(W, 1)
-    w = W(i,:);
-    
-    AX_sum_W = [AX_sum_W; AX + ones(size(AX,1),length(w)) * w'];
+% AX + BU
+BU = U * B';
+[BU_row, BU_col] = size(BU);
+
+AX_sum_BU = [];
+for i = 1:AX_row
+     for j = 1:BU_row
+          AX_sum_BU = [AX_sum_BU; AX(i,:) + BU(j,:)];
+     end
 end
 
-BU = U * B';
-AX_sum_W_sum_BU = [];
-for i = 1:size(BU, 1)
+AX_sum_BU = AX_sum_BU(convhull(AX_sum_BU),:);
+AX_sum_BU = unique(AX_sum_BU, 'rows');
+[AX_sum_BU_row, AX_sum_BU_col] = size(AX_sum_BU);
 
-     bu = BU(i,:);
-     AX_sum_W_sum_BU = [AX_sum_W_sum_BU; AX_sum_W + ones(size(AX_sum_W,1),length(bu)) * bu'];
-end  
+% AX_sum_BU_sum_W
+W = con2vert(H_w, h_w);
+[W_row, W_col] = size(W);
 
-AX_sum_W_sum_BU = AX_sum_W_sum_BU(convhull(AX_sum_W_sum_BU),:);
-p_AX_sum_W_sum_BU = polyshape(AX_sum_W_sum_BU);
-plot(p_AX_sum_W_sum_BU);
+AX_sum_BU_sum_W = [];
+for i = 1:AX_sum_BU_row
+     for j = 1:W_row
+          AX_sum_BU_sum_W = [AX_sum_BU_sum_W; AX_sum_BU(i,:) + W(j,:)];
+     end
+end
 
+AX_sum_BU_sum_W = AX_sum_BU_sum_W(convhull(AX_sum_BU_sum_W),:);
+% AX_sum_BU_sum_W = unique(AX_sum_BU_sum_W, 'rows');
+p_AX_sum_BU_sum_W = polyshape(AX_sum_BU_sum_W);
+plot(p_AX_sum_BU_sum_W);
 
+legend(["$\mathcal{X}$", "$Pre(\mathcal{X}, \mathcal{U}, \mathcal{W})$", "$Pre(\mathcal{X}, \mathcal{U}, \mathcal{W}) \cap \mathcal{X}$", "$Suc(\mathcal X, \mathcal W)$"], 'interpreter', 'latex');
